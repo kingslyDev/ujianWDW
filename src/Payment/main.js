860dd142fe6d6af0cd5dc9c57b06976c0a62a742
arasell <achmadrisel@gmail.com>
2023-11-16 23:16:31 +0700

    ngepush

---
+++ src/Payment/main.js
@@ -0,0 +1,28 @@
+const eMoney = document.querySelector(".emoney");
+const isBank = document.querySelector(".bank");
+const eWallet = document.querySelector(".ewallet");
+const isBanking = document.querySelector(".atm");
+const MoneyArrow = eWallet.querySelector(".bi-caret-up");
+const BankArrow = isBanking.querySelector(".bi-caret-up");
+
+const toggleSwitch = (element, arrow) => {
+  const hiddenElement =
+    element.style.display === "none" || element.style.display === "";
+  if (hiddenElement) {
+    arrow.classList.remove("bi-caret-up");
+    arrow.classList.add("bi-caret-down");
+    element.style.display = "block";
+  } else {
+    arrow.classList.remove("bi-caret-down");
+    arrow.classList.add("bi-caret-up");
+    element.style.display = "none";
+  }
+};
+
+MoneyArrow.addEventListener("click", () => {
+  toggleSwitch(eMoney, MoneyArrow);
+});
+
+BankArrow.addEventListener("click", () => {
+  toggleSwitch(isBank, BankArrow);
+});
